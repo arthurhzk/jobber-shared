4851644846d033d47322c2351d26a5db5203ba26
Arthur Henrique Zarpelon Kunz <arthurhkunz@MacBook-Air-de-Arthur.local>
2024-09-13 22:37:17 -0300

    add server.ts file

---
+++ server/2-notification-service/src/server.ts
@@ -0,0 +1,31 @@
+import 'express-async-errors';
+import http from 'http';
+
+import { Logger } from 'winston';
+import { config } from '@notifications/config';
+import { winstonLogger } from '@notifications/logger';
+import { Application } from 'express';
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationServer', 'debug');
+const SERVER_PORT = 4001;
+
+export function start(app: Application): void {
+  startServer(app);
+  startQueues();
+  startElasticSearch();
+}
+
+async function startQueues(): Promise<void> {}
+
+function startElasticSearch(): void {}
+
+function startServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(`Worker with process id of ${process.pid} on notification server has started`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log('error', 'NotificationService startServer() method:', error);
+  }
+}
